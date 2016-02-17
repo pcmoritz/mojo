faf496d17f4d7244c6bad88b80cd59dcf084bbd9
Mitch Rudominer <rudominer@chromium.org>
2016-02-17 12:50:43 -0800

    Delete version 1 of the Mojom parser from the source tree.
    BUG=#671
    R=azani@chromium.org
    
    Review URL: https://codereview.chromium.org/1708643003 .

--- mojo/public/tools/bindings/run_code_generators.py
+++ mojo/public/tools/bindings/run_code_generators.py
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# This script accepts the output of version 2 of the mojom parser and uses that
+# This script accepts the output of the mojom parser and uses that
 # data to invoke the code generators.
 
 import argparse
